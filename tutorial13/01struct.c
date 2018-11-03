d6191251debb6dcb9311bd8a1fd27d21f8e70c70
zhuhui1996 <13869841543@163.com>
2018-11-03 11:30:47 +0800

    Modified file 01struct.c

--- tutorial13/01struct.c
+++ tutorial13/01struct.c
@@ -20,12 +20,27 @@ typedef struct //person
 int main()
 {
 	//struct person Zhangzeyu;
-	sperson Zhangzeyu;
-	Zhangzeyu.age=20;
-	Zhangzeyu.height=180;
-	Zhangzeyu.name[20]="Zhang Zeyu";
+	sperson Zhangzeyu = {20,1.80f,"ZhangZeyu"};
+	Zhangzeyu.age = 20;
+	Zhangzeyu.height = 180;
+	Zhangzeyu.name[20] = "Zhang Zeyu";
 	printf("Zhangzeyu's info:\n");
 	printf("age:%d\n",Zhangzeyu.age);
 
+	sperson person1;
+	printf("请输入年龄：");
+	scanf("%d",&(person1.age));
+	printf("请输入身高：");
+	scanf("%g",&(person1.height));
+	scanf("%*[^\n]");
+	scanf("%*c");
+
+	printf("请输入姓名：");
+	fgets(person1.name,20,stdin);
+
+	
+	printf("姓名是：%s",person1.name);
+	printf("年龄是：%d\n",person1.age);
+	printf("身高是：%g\n",person1.height);
 	return 0;
 }
