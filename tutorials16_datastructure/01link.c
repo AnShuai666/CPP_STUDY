f190817137d1ec838d2aacf20e10d9e34ed03e5c
AnShuai666 <1028792866@qq.com>
2018-11-26 13:38:12 +0800

    Add file 01link.c 线性链表练习 插入删除 排序 随机访问 计数

--- tutorials16_datastructure/01link.c
+++ tutorials16_datastructure/01link.c
@@ -11,7 +11,8 @@ typedef struct node
 } node;
 
 int main()
-{	
+{
+	int cnt = 0;	
 	node *p_node = NULL;
 	node node1 = {1};
 	node node2 = {2};
@@ -70,5 +71,34 @@ int main()
 	}
 	printf("\n");
 	
+	for(p_node = &head; p_node != &tail; p_node = p_node->p_next)
+	{
+		node *p_first = p_node;
+		node *p_mid = p_first->p_next;
+		node *p_last = p_mid->p_next;
+
+		if(p_mid != &tail)
+		{
+			cnt++;
+		}
+	}
+	printf("有效数字个数是%d\n",cnt);
+	
+	cnt = 0;
+	for(p_node = &head; p_node != &tail; p_node = p_node->p_next)
+	{
+		node *p_first = p_node;
+		node *p_mid = p_first->p_next;
+		node *p_last = p_mid->p_next;
+		if(p_mid != &tail)
+		{
+			if(cnt == 2)
+			{
+				printf("编号为2的数字是:%d\n",p_mid->num);
+				break;
+			}
+		}
+		cnt++;
+	}
 	return 0;
 } 
