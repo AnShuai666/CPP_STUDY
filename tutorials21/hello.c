c96fc4b221ebeb77e402f7898b0c406e7675005c
AnShuai666 <1028792866@qq.com>
2018-12-19 16:26:25 +0800

    更新

---
+++ tutorials21/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//#include "a.txt"
+//fhdjj
+/*
+efuhi
+*/
+
+int main(void)
+{
+	//fjreijf
+	printf("hello world!\n");
+	return 0;
+}
---
+++ tutorials21/tmath/tmath.h
@@ -0,0 +1,14 @@
+#ifndef T_MATH_H
+#define T_MATH_H
+/*函数的声明*/
+/*宏定义*/
+/*文件的包含*/
+/*类型的定义*/
+/*变量的声明*/
+int add(int num1, int num2);
+int sub(int num1, int num2);
+
+int mul(int num1, int num2);
+int div(int num1, int num2);
+
+#endif
