210bd6d8948dde35f5d61c4856c67d3fb25e1441
AnShuai666 <1028792866@qq.com>
2018-12-21 23:16:54 +0800

    更新

---
+++ tutorials23/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "tmath/tmath.h"
+typedef int (*p_func[4])(int,int);
+int main()
+{
+	int a = 6, b = 2;
+	p_func p = {t_add,t_sub,t_mul,t_div};
+	for(int i = 0; i < 4; i++)
+	{
+		printf("%d\n",p[i](a,b));
+	}
+	return 0;
+}
---
+++ tutorials23/tmath/add.c
@@ -0,0 +1,9 @@
+int t_add(int a, int b)
+{
+	return a + b;
+}
+
+int t_sub(int a, int b)
+{
+	return a - b;
+}
